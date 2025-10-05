404ed36f47bffa91dcfe1447141b2b52be9b831c
AlwxxxKUHf <bdjdbskskss@outlook.com>
2025-10-05 13:06:20 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -494,4 +494,4 @@ def login_http():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
     logging.info(f"Starting server on port {port}")
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
